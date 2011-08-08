ea584703d2eb6d1e147061fd0012ea78fd3e5b58
Isaac Barnes <ikebarnes@beatcreators.com>
2011-08-08 08:25:37 -0400

    Added Toms email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
    def checkin(clock)
      @clock = clock
-    mail(:to => 'isaac.barnes@med.navy.mil,jose.risi@med.navy.mil',
+    mail(:to => 'isaac.barnes@med.navy.mil,jose.risi@med.navy.mil,thomas.hormby.ctr@med.navy.mil ',
       :subject => clock.user.name + " check " + clock.status.downcase + " at " + Time.now.strftime("%I:%M %p"),
       :from => "noreply@theteamtime.com")
   end
